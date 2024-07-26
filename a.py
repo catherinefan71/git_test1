e1569e52ca90c1fe90fae20ba63503443cb12f0e
catherinefan71 <2868890442@qq.com>
2024-07-27 11:14:41 +1200

    Paris

--- a.py
+++ a.py
@@ -12,5 +12,6 @@ def main():
     print("I am developing c1 Project")
     print("in the local-2 branch")
     print("I am developing c1 Project")   
-    print("I am in gitHub local-1")   
+    print("I am in gitHub local-1")  
+    print("Paris")
 main()
